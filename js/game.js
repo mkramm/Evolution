596f57b845563a0efa79f4e3c00e92a603a9f1dd
Krammi <markuskramm@gmail.com>
2016-03-24 10:43:23 +0100

    #2 separate Button and count

--- js/game.js
+++ js/game.js
@@ -1,33 +1,68 @@
 
-var Archaeon = React.createClass({
-  getInitialState: function () {
-    return {
-      value: 0
-    }
-  },
-  handleClick: function (){
-    this.setState(function(state) {
-      return {value: state.value + 1};
-    })
-  },
-  render: function() {
-    var text = 'Archaeen';
-    return <div>
-      <button onClick={this.handleClick}>{text}</button>
-      <div>
-        {this.state.value}
-      </div>
-    </div>;
-  }
-});
 var start = new Date().getTime();
 setInterval(function() {
   ReactDOM.render(
-    <Archaeon />,
-    document.getElementById('archaeon')
+    <App />,
+    document.getElementById('container')
   );
 }, 50);
-ReactDOM.render(
-  <span></span>,
-  document.getElementById('container')
-);
+
+
+
+var AmountView = React.createClass({
+  render: function () {
+    var className = 'amount' + this.props.internalId.charAt(0).toUpperCase() + this.props.internalId.slice(1);
+    return <div className={className}>
+      {this.props.amount}
+    </div>;
+  }
+});
+
+var FeatureButton = React.createClass({
+  render: function () {
+    var className = 'button' + this.props.internalId.charAt(0).toUpperCase() + this.props.internalId.slice(1);
+    return <button onClick={this.props.handleClick} className={className}>{this.props.text}</button>;
+  }
+});
+
+
+
+var App = React.createClass({
+  getInitialState: function () {
+    return {data:[
+      {
+        internalId: 'archaeen',
+        text: 'Archaeon',
+        value: 0,
+        handleClick: function (a,b,c,d) {
+          this.setState(function (state) {
+            console.log(state);
+          });
+        }
+      }
+    ]};
+  },
+  handleButtonClick: function(i) {
+    this.setState(function () {
+      this.state
+      return {}
+    });
+  },
+  render: function () {
+    var amount = 0;
+    var internalId = 'Archaeen';
+
+    return <div id="App">
+      <div id="amountContainer">
+        {this.state.data.map(function(result, i) {
+          return <AmountView amount={result.value} internalId={result.internalId} key={i} />
+        })}
+      </div>
+      <div id="buttonContainer">
+        {this.state.data.map(function(result, i) {
+          return <FeatureButton handleClick={result.handleClick.bind(this, i)} internalId={result.internalId} text={result.text} key={i} />
+        }.bind(this))}
+    </div>
+    </div>;
+  }
+});
