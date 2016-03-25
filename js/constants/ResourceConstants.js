d30d0bd888077b8fdcc523c74e6920bdf29eeb37
Krammi <markuskramm@gmail.com>
2016-03-25 23:36:29 +0100

    #2 remove unnecessary Constants

--- js/constants/ResourceConstants.js
+++ js/constants/ResourceConstants.js
@@ -3,13 +3,5 @@
 var keyMirror = require('keymirror');
 
 module.exports = keyMirror({
-  RESOURCE_CREATE: null,
-  RESOURCE_COMPLETE: null,
-  RESOURCE_DESTROY: null,
-  RESOURCE_DESTROY_COMPLETED: null,
-  RESOURCE_TOGGLE_COMPLETE_ALL: null,
-  RESOURCE_UNDO_COMPLETE: null,
-  RESOURCE_UPDATE_TEXT: null,
-
   RESOURCE_INCREASE: null
 });
